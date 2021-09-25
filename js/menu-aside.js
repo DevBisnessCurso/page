9788d7465867b7f2a6bbea02f12d775614c233a9
Julio Quiñones <obtenerrecursos@gmail.com>
2021-09-25 11:01:27 -0500

    CAMBIOS EN LA SECCION DEL TALLER REPRODUCTOR DE MUSICA CON HTML, CSS, JS

--- js/menu-aside.js
+++ js/menu-aside.js
@@ -2,6 +2,7 @@ const navigation = qS('.content-navigations-aside')
 const show = qS('.button-menu')
 const showdropdown = qS('.menu-aside')
 const icondropdown = qS('.icon-dropdown-menu')
+const closeAutoMenu = qS('.menu-aside')
 
 navigation.addEventListener('click', e => {
     if(e.target.tagName === "DIV" && e.target.classList.contains('content-navigations-aside')) {
@@ -18,3 +19,12 @@ showdropdown.addEventListener('click', e => {
         e.target.children[0].classList.toggle('icon-dropdown-rotate')
     }
 })
+
+let  mediaqueryAside = window.matchMedia("(max-width: 640px)");
+mediaqueryAside.addListener(EventoMediaQueryList => {
+    closeAutoMenu.addEventListener('click', e => {
+        if(e.target.classList.contains('submenu-link-aside') && e.target.tagName === "A"){
+            navigation.classList.toggle('aside-open')
+        }
+    })
+});
